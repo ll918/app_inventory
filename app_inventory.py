1dfd55a3f3cfbffa32b0c80d2a84dcca40487a3e
ll918 <ll918@users.noreply.github.com>
2017-12-27 19:19:43 -0500

    Rewrote the whole thing.
    Now reading version from property file instead of metadata.

--- app_inventory.py
+++ app_inventory.py
@@ -2,14 +2,16 @@
 """
 On OSX, build an inventory of applications found in the Applications directory.
 
-The inventory contains the name, path and version of the applications.  The
-version data is obtained with the 'mdls' command.
+The inventory contains the name, path and version of the applications.
 
 The data gathered is save to json file.
+
+https://docs.python.org/3/library/plistlib.html
+https://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man5/plist.5.html
 """
 import json
 import os
-import subprocess
+import plistlib
 
 app_dir = '/Applications'
 inventory = {}
@@ -21,12 +23,13 @@ def build_inventory(app_path):
 
     for item in os.scandir(app_path):
         if item.name.endswith('.app'):
-            path = clean_path(item.path)
+            path = item.path
             try:
-                version = os_command_str(
-                    'mdls -name kMDItemVersion -raw ' + path)
-                if version == '(null)':
-                    version = 'n/a'
+                with open(os.path.join(path, 'Contents/Info.plist'),
+                          'rb') as f:
+                    properties = plistlib.load(f)
+                version = properties.get('CFBundleShortVersionString', 'n/a')
+
                 inventory.update(
                     {item.name: {'path': path, 'version': version}})
             except Exception as e:
@@ -36,34 +39,10 @@ def build_inventory(app_path):
                 new_path = os.path.join(app_path, item.name)
                 if os.path.isdir(new_path):
                     build_inventory(new_path)
-
-
-def clean_path(app_path):
-    """ Add escape character('\') to file names.
-
-    Spaces and other characters in path can cause problems when executing os
-    command if not 'escaped'.
-    """
-
-    if ' ' in app_path:
-        app_path = '\ '.join(app_path.split())
-    if '(' in app_path:
-        app_path = '\('.join(app_path.split('('))
-    if ')' in app_path:
-        app_path = '\)'.join(app_path.split(')'))
-    return app_path
-
-
-def os_command_str(command):
-    # run an OS command (string) and return output as a string.
-
-    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-    (output, err) = p.communicate()
-    return output.decode()
+    if inventory:
+        with open(file, 'w') as f:
+            json.dump(inventory, f)
+    return
 
 
 build_inventory(app_dir)
-
-if inventory:
-    with open(file, 'w') as f:
-        json.dump(inventory, f)
